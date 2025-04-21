72b50f0cb98ceccc21cea01d023d7fae9693fe42
Seulah Lee <leeseula0123@gmail.com>
2025-04-21 22:19:49 +0900

    [Silver III] Title: 바이러스, Time: 56 ms, Memory: 34924 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -1,26 +1,28 @@
 from collections import deque
 
 N = int(input()) # 컴퓨터 수
-M = int(input()) # 연결 수
-graph = [[] for _ in range(N+1)] # 인덱스 0 안씀(인덱스 = 컴퓨터 번호)
-visited = [0]*(N+1)
+K = int(input()) # 페어 수
 
-for _ in range(M):
+computers = [[] for _ in range(N+1)]
+visited =[0]*(N+1)
+
+for _ in range(K):
     x,y = map(int,input().split())
-    graph[x].append(y)
-    graph[y].append(x) # 양방향 연결
+    computers[x].append(y)
+    computers[y].append(x) #양방향 연결
 
 def bfs(start):
+    cnt = 0
     queue = deque([start])
     visited[start] = 1
-    cnt = 0
+
     while queue:
         cur = queue.popleft()
-        for a in graph[cur]:
-            if not visited[a]:
-                queue.append(a)
-                visited[a] = 1
-                cnt +=1
+        for computer in computers[cur]:
+            if not visited[computer]:
+                visited[computer]=1
+                queue.append(computer)
+                cnt+=1
     return cnt
 
 print(bfs(1))
