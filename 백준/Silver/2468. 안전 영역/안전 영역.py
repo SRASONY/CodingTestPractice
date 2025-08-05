7865fb41935fafa5c0826a584595eaff787bcbf1
Seulah Lee <leeseula0123@gmail.com>
2025-08-05 22:15:35 +0900

    [Silver I] Title: 안전 영역, Time: 676 ms, Memory: 34992 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.py
+++ 백준/Silver/2468. 안전 영역/안전 영역.py
@@ -1,35 +1,38 @@
 from collections import deque
 
 N = int(input())
-location = [list(map(int,input().split())) for _ in range(N)]
-height = max(map(max,location))
-cnt_list = []
+graph = [list(map(int,input().split())) for _ in range(N)]
+answer = []
 
-dx = [-1,1,0,0]
-dy = [0,0,-1,1]
+heights = set()
+heights.add(0)
+for i in range(N):
+    for j in range(N):
+        heights.add(graph[i][j])
 
-def bfs(i,j,visited,h):
-    queue = deque([(i,j)])
+def bfs(x,y,a):
+    queue = deque([(x,y)])
+    visited[x][y] = 1
+
+    dx = [-1,1,0,0]
+    dy = [0,0,-1,1]
     while queue:
         x,y = queue.popleft()
-        visited[x][y] = 1
         for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            if 0<=nx<N and 0<=ny<N:
-                if not visited[nx][ny] and location[nx][ny] > h:
-                    visited[nx][ny] =1
-                    queue.append((nx,ny))
+            nx = dx[i] + x
+            ny = dy[i] + y
+            if 0<=nx<N and 0<=ny<N and visited[nx][ny]==0 and graph[nx][ny]>a:
+                visited[nx][ny] = 1
+                queue.append((nx,ny))
 
-for h in range(0,height+1):
-    visited = [[0]*N for _ in range(N)]
-    cnt = 0
+for h in heights:
+    cnt =0
+    visited = [[0]* N for _ in range(N)]
     for i in range(N):
         for j in range(N):
-            # 아직 방문 안했고 잠기지 않았다면 새 영역의 시작점
-            if not visited[i][j] and location[i][j] > h:
-                bfs(i,j,visited,h)
+            if visited[i][j]==0 and graph[i][j]>h:
+                bfs(i,j,h)
                 cnt +=1
-    cnt_list.append(cnt)
+    answer.append(cnt)
 
-print(max(cnt_list))
+print(max(answer))
